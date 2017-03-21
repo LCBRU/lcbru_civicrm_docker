05c38100bd473632a86e6f15c3ee70604e821327
Richard Bramley <rabramley@gmail.com>
2017-03-21 09:52:32 +0000

    Get working

--- civicrm/bin/install_drupal_modules.sh
+++ civicrm/bin/install_drupal_modules.sh
@@ -4,40 +4,47 @@
 
 sleep 30
 
-cd ..
-rm -fr html
-
-# Configue Drupal
-drush dl drupal-7.54
-mv drupal-7.54 html
-cd html
 cp /info.php .
 
+# Setup Drupal
 drush si --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@mysql/${MYSQL_DRUPAL_DATABASE} --locale=uk --account-mail=${DRUPAL_ACCOUNT_MAIL} --account-name=${DRUPAL_ACCOUNT_NAME} --account-pass=${DRUPAL_ACCOUNT_PASS} --site-mail=${DRUPAL_SITE_MAIL} --site-name="${DRUPAL_SITE_NAME}" -y
 
-# Install CiviCRM
-ln -s /civicrm/civicrm /var/www/html/sites/all/modules/
-
+# Setup CiviCRM
 drush --include=sites/all/modules/civicrm/drupal/drush civicrm-install --dbname=civicrm --dbpass=${MYSQL_PASSWORD} --dbuser=${MYSQL_USER} --dbhost=mysql --destination=sites/all/modules --site_url=localhost
 
-drush dl ctools datatables devel views -y
+## Enable the module
 drush pm-enable ctools -y
 drush pm-enable devel -y
 drush pm-enable views -y
 drush pm-enable datatables -y
+drush pm-enable dblib -y
 
 drush up
 
+# Link to the LCBRU modules and custom code
 ln -s /lcbru_civicrm/lcbru_custom /var/www/html/sites/all/
 ln -s /lcbru_civicrm/lcbru_modules /var/www/html/sites/all/modules/
-
-chown -R www-data:www-data sites
 chown -R www-data:www-data /lcbru_civicrm
-chown -R www-data:www-data /civicrm
 
-drush pm-enable lcbru -y
+# Set up external databases
+ln -s /lcbru_civicrm/settings/external_databases.php /var/www/html/sites/default/
+printf "\n\ninclude 'external_databases.php';\n\n" >> /var/www/html/sites/default/settings.php
 
-chown -R www-data:www-data sites
+# Set the locations of the custom code directories
+drush cvapi Setting.create sequential=1 customPHPPathDir="/var/www/html/sites/all/lcbru_custom/civicrm_php/"
+drush cvapi Setting.create sequential=1 customTemplateDir="/var/www/html/sites/all/lcbru_custom/civicrm_templates/"
+drush cvapi Setting.create sequential=1 extensionsDir="/var/www/html/sites/all/lcbru_custom/civicrm_extensions/" 
 
+
+# Enable each of the modules in the semi-colon
+# delimited list of packages in the ENABLED_PACKAGES
+# environment variable provided by docker-compose.yml
+IFS=';' read -ra PACKAGES <<< "$ENABLED_PACKAGES"
+for i in "${PACKAGES[@]}"; do
+	echo "----- $i ------"
+	drush pm-enable $i -y
+done
+
+# Start apache with the rewrite module enabled
 a2enmod rewrite
 apache2-foreground
